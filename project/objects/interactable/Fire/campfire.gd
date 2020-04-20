b17475ece64b08e69d3e3628d509704f05a29c67
Caleb Pentecost <caleb.james.pentecost@gmail.com>
2020-04-20 15:46:22 -0700

    Bring the fire's brightness all the way up to the campfire fuel level

--- project/objects/interactable/Fire/campfire.gd
+++ project/objects/interactable/Fire/campfire.gd
@@ -4,6 +4,10 @@ var decay_enabled = false
 var decay = 2 # Units of fuel per second
 var current_fuel = 100
 
+signal fuel_ran_out
+
+var fuels_out = false
+
 func _ready():
 	set_process(true)
 
@@ -12,9 +16,18 @@ func _process(delta):
 	if decay_enabled:
 		current_fuel -= delta * decay
 		
+		if current_fuel <= 0:
+			current_fuel = 0
+			if fuels_out == false:
+				fuels_out = true
+				emit_signal("fuel_ran_out")
+		 
+		
 	# Modify campfire things
+	$CrispyLogs.fire_brightness = current_fuel / 100 
 	
 
 func add_fuel():
 	decay_enabled = true
+	fuels_out = false
 	current_fuel += 100
